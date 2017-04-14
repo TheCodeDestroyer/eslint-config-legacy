b7351e58ccf65db9441fedbf80948ca3a9a979e9
Nace Logar <the.code.destroyer@gmail.com>
2017-04-14 10:19:37 +0200

    Created esnext config

---
+++ configs/esnext.js
@@ -0,0 +1,23 @@
+module.exports = {
+    'extends': 'tcd',
+    'parserOptions': {
+        'ecmaVersion': 8,
+        'ecmaFeatures': {
+            'experimentalObjectRestSpread': true
+        },
+        'sourceType': 'module'
+    },
+    'env': {
+        'es6': true
+    },
+    'rules': {
+        'no-var': 'error',
+        'prefer-const': 'error',
+        'one-var': ['error', 'never'],
+        'prefer-arrow-callback': 'error',
+        'prefer-numeric-literals': 'error',
+        'prefer-rest-params': 'error',
+        'prefer-spread': 'error',
+        'prefer-template': 'error'
+    }
+};
